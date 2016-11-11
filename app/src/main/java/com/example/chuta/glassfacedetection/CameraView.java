3fe12046bedd6d5beb6cb06cabdf90ef3b26295b
Chuta Sano <chuta_japan@comcast.net>
2016-11-11 18:56:37 -0500

    Face Recognition

---
+++ app/src/main/java/com/example/chuta/glassfacedetection/CameraView.java
@@ -0,0 +1,34 @@
+package com.example.chuta.glassfacedetection;
+
+
+
+import org.opencv.android.JavaCameraView;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.util.AttributeSet;
+
+public class CameraView extends JavaCameraView
+{
+    public CameraView(Context context, int cameraId) {
+        super(context, cameraId);
+    }
+
+    public CameraView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected boolean initializeCamera(int width, int height)
+    {
+        super.initializeCamera(width, height);
+
+        Camera.Parameters params = mCamera.getParameters();
+
+        // Post XE10 Hotfix
+        params.setPreviewFpsRange(30000, 30000);
+        mCamera.setParameters(params);
+
+        return true;
+    }
+}
